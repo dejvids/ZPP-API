f8fc495396cdaca3a11b6fae18ca7e6edc11bc71
dawid_surys <dawid.surys@hotmail.com>
2019-02-10 18:42:59 +0100

    Added DBContext and set connection params
    Related Work Items: #24, #25

---
+++ ZPP.Server/Models/MainDbContext.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ZPP.Server.Entities;
+
+namespace ZPP.Server.Models
+{
+    public class MainDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<Lecture> Classes { get; set; }
+        public DbSet<Participant> Participants { get; set; }
+        public DbSet<Opinion> Opinions { get; set; }
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<VerificationCode> VerificationCodes { get; set; }
+
+        public MainDbContext(DbContextOptions<MainDbContext> options)
+       : base(options)
+        { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Participant>().HasKey(p => new { p.StudentId, p.LectureId });
+            modelBuilder.Entity<User>().HasData(
+                new User { Id = 1, Login = "dsurys", Email = "dawid.surys@pollub.edu.pl", IsActive = true, Name = "Dawid", Surname = "Suryś" });
+
+            modelBuilder.Entity<Role>().HasData(
+                new Role { Id = 1, Name = "admin" },
+                new Role { Id = 2, Name = "student" },
+                new Role { Id = 3, Name = "lecturer" },
+                new Role { Id = 4, Name = "company" });
+        }
+    }
+}
--- ZPP.Server/Startup.cs
+++ ZPP.Server/Startup.cs
@@ -6,10 +6,12 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using ZPP.Server.Models;
 
 namespace ZPP.Server
 {
@@ -25,6 +27,15 @@ namespace ZPP.Server
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<MainDbContext>(options => options.UseSqlServer(Configuration.GetConnectionString("local")));
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy", cors =>
+                    cors.AllowAnyOrigin()
+                    .AllowAnyMethod()
+                    .AllowAnyHeader()
+                    .AllowCredentials());
+            });
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
@@ -40,7 +51,7 @@ namespace ZPP.Server
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
+            app.UseCors("CorsPolicy");
             app.UseHttpsRedirection();
             app.UseMvc();
         }
