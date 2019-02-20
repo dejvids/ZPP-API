247d62da4284fad1450cfe21db0b611482d417c7
dawid_surys <dawid.surys@hotmail.com>
2019-02-20 23:43:58 +0100

    Blazor setup

---
+++ ZPP.Blazor/Models/User.cs
@@ -0,0 +1,10 @@
+namespace ZPP_Blazor.Models
+{
+    public class User
+    {
+        public string Login { get; set; }
+        public string Name { get; set; }
+        public string Surname { get; set; }
+        public string Email { get; set; }
+    }
+}
--- ZPP.Blazor/Startup.cs
+++ ZPP.Blazor/Startup.cs
@@ -1,12 +1,17 @@
+using Blazor.Extensions.Storage;
 using Microsoft.AspNetCore.Blazor.Builder;
 using Microsoft.Extensions.DependencyInjection;
+using ZPP.Blazor;
 
-namespace ZPP.Blazor
+namespace ZPP_Blazor
 {
     public class Startup
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            // Add Blazor.Extensions.Storage
+            // Both SessionStorage and LocalStorage are registered
+            services.AddStorage();
         }
 
         public void Configure(IBlazorApplicationBuilder app)
