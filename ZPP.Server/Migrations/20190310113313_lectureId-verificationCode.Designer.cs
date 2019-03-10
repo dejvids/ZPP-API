fc3c1bb86962a6559f09c248b9f6eca689e9cdb7
dawid_surys <dawid.surys@hotmail.com>
2019-03-10 18:37:59 +0100

    Added API to generate verification code, using endpoint to set user as present
    Related Work Items: #49

--- ZPP.Server/Migrations/20190310113313_lectureId-verificationCode.Designer.cs
+++ ZPP.Server/Migrations/20190310113313_lectureId-verificationCode.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ZPP.Server.Models;
 
 namespace ZPP.Server.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190310113313_lectureId-verificationCode")]
+    partial class lectureIdverificationCode
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
