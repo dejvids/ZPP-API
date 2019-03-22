598962a10229d04faaa941ca5aa7f596363ea46e
dawid_surys <dawid.surys@hotmail.com>
2019-03-22 23:33:55 +0100

    Working Homepage

--- ZPP_Blazor/Components/Home/HomeComponent.cs
+++ ZPP_Blazor/Components/Home/HomeComponent.cs
@@ -3,25 +3,45 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Blazor.Components;
 using Microsoft.JSInterop;
 using System.Linq;
+using System;
+using System.Threading.Tasks;
 
 namespace ZPP_Blazor.Components.Home
 {
-    public class HomeComponent: BaseComponent
+    public class HomeComponent : BaseComponent
     {
-        public List<Lecture> Lectures { get; set; } 
+        public List<Lecture> Lectures { get; set; }
         public List<Lecture> PromotingLectures { get; set; }
 
-        protected override void OnInit()
+        public HomeComponent()
+        { }
+
+        protected override async Task OnInitAsync()
         {
-            //var response = Http.GetAsync("/api/lectures").Result;
-            //if(response.StatusCode == System.Net.HttpStatusCode.OK)
-            //{
-            //    Lectures = Json.Deserialize<List<Lecture>>(response.Content.ReadAsStringAsync().Result);
-            //    if(Lectures.Count >= 3)
-            //    {
-            //        PromotingLectures = Lectures.Take(3).ToList();
-            //    }
-            //}
+            PromotingLectures = new List<Lecture> { new Lecture(), new Lecture(), new Lecture() };
+            await base.OnInitAsync();
+            Console.WriteLine("OnInit");
+            if (Http == null)
+            {
+                Console.WriteLine("Http is null");
+            }
+            Console.WriteLine(AppCtx.BaseAddress);
+            var response = await Http.GetAsync(AppCtx.BaseAddress + "/api/lectures");
+            if (response == null)
+            {
+                Console.WriteLine("Result is null");
+            }
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+            {
+                Console.WriteLine(await response.Content.ReadAsStringAsync());
+
+                Lectures = Json.Deserialize<List<Lecture>>(response.Content.ReadAsStringAsync().Result);
+                Console.WriteLine("Pobranych wyk³adów " + Lectures.Count);
+                if (Lectures.Count >= 3)
+                {
+                    PromotingLectures = Lectures.Take(3).ToList();
+                }
+            }
         }
     }
 }
